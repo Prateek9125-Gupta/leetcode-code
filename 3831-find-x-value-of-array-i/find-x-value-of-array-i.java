76507865950360823be2487e3039ec8538ed8c1e
Prateek Gupta <pg5768523@gmail.com>
2026-09-21 11:39:49 +0530

    Time: 12 ms (92.31%) | Memory: 93.5 MB (48.08%) - LeetSync

---
+++ 3831-find-x-value-of-array-i/find-x-value-of-array-i.java
@@ -0,0 +1,37 @@
+class Solution {
+    public long[] resultArray(int[] nums, int k) {
+
+        long[] result = new long[k];
+
+        // dp[r] = number of subarrays ending at previous position
+        // whose product % k == r
+        long[] dp = new long[k];
+
+        for (int num : nums) {
+
+            long[] newDp = new long[k];
+
+            int val = num % k;
+
+            // Start a new subarray containing only nums[i]
+            newDp[val]++;
+
+            // Extend every previous subarray
+            for (int r = 0; r < k; r++) {
+                if (dp[r] > 0) {
+                    int newRemainder = (r * val) % k;
+                    newDp[newRemainder] += dp[r];
+                }
+            }
+
+            // All subarrays ending here contribute to answer
+            for (int r = 0; r < k; r++) {
+                result[r] += newDp[r];
+            }
+
+            dp = newDp;
+        }
+
+        return result;
+    }
+}
